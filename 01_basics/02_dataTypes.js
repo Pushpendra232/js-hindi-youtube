b8f27767dabce8faf18979a8f22921fe407bdab5
pushpendra232 <Pushpendra.Nayak2@ksb.com>
2025-06-16 19:35:37 +0530

    learn data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js  code as newer version
+
+//alert(3 + 3) - we are using node js not a browser, so alert is not available
+
+ console.log(3+3)  // code readablity should be high.
+ 
+ console.log("Hello World!")
+
+
+ let name = "Pushpendra"
+ let age = 30
+ let isMarried = false
+ let city = null
+ let state 
+
+ // number => 2 to power 53
+ // bigint => 2 to power 53 + 1
+ // string => "Pushpendra"
+ // boolean => true or false
+ // null => no value  // is standard value
+ // undefined => variable is declared but not assigned a value
+ // symbol => used to create unique identifiers
+ // object => collection of key-value pairs
+
+ console.log(typeof null) // string
+    console.log(typeof undefined) // undefined
